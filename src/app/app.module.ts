e00fe87dc6aad3dee9dd9828e4892542de31d1f7
imobac-marin <imomarin@gmail.com>
2018-02-26 09:12:50 +0000

    Clase nº6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { FechaactualComponent } from './fechaactual/fechaactual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaactualComponent
   ],
   imports: [
     BrowserModule
