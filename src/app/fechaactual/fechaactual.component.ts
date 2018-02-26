1004a329ab0c5ce972405ce9f9ea3a80c996941b
imobac-marin <imomarin@gmail.com>
2018-02-26 14:56:35 +0000

    Clase nº27

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-fechaactual',
     template: `
-        <p>Madrid, {{ hoy | date: 'd/M/y H:m'}}</p>
+        <p>Madrid, {{ hoy | date: 'd-M-y'}} a las {{ hoy | date: 'H:m Z'}}</p>
         <app-copyright></app-copyright>
     `,
     styleUrls: ['./fechaactual.component.css']
