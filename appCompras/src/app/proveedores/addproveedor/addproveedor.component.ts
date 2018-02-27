172a0f6ad8180c056abf77890474cf79014b9547
imobac-marin <imomarin@gmail.com>
2018-02-27 10:07:31 +0000

    Clase nº38

--- appCompras/src/app/proveedores/addproveedor/addproveedor.component.ts
+++ appCompras/src/app/proveedores/addproveedor/addproveedor.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-addproveedor',
   templateUrl: './addproveedor.component.html',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproveedorComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formprov') formprov: NgForm;
+  proveedor: any;
+
+  constructor() {
+    this.proveedor = {
+      nombre: '',
+      cif: '',
+      direccion: '',
+      cp: '',
+      localidad: '',
+      provincia: '',
+      telefono: null,
+      email: '',
+      contacto: ''
+    };
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.proveedor.nombre = this.formprov.value.nombre;
+    this.proveedor.cif = this.formprov.value.cif;
+    this.proveedor.direccion = this.formprov.value.direccion;
+    this.proveedor.cp = this.formprov.value.cp;
+    this.proveedor.localidad = this.formprov.value.localidad;
+    this.proveedor.provincia = this.formprov.value.provincia;
+    this.proveedor.telefono = this.formprov.value.telefono;
+    this.proveedor.email = this.formprov.value.email;
+    this.proveedor.contacto = this.formprov.value.contacto;
+
+    this.formprov.reset();
   }
 
 }
