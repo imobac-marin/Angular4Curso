252bfa54fa850a4ae74dc7eb9e204c7001fd36d5
imobac-marin <imomarin@gmail.com>
2018-02-27 11:13:48 +0000

    Clase nº42

--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
@@ -9,11 +9,13 @@ import { ProveedoresComponent } from './proveedores/proveedores/proveedores.comp
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
 import { AddproveedorComponent } from './proveedores/addproveedor/addproveedor.component';
+import { AddpresupuestoComponent } from './presupuestos/addpresupuesto/addpresupuesto.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent }, // Ruta de inicio
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'addproveedor', component: AddproveedorComponent },
+  { path: 'addpresupuesto', component: AddpresupuestoComponent },
   { path: '**', component: InicioComponent } // Redirige a la página de inicio si no es capaz de resolver la ruta.
 ];
 
@@ -23,12 +25,14 @@ const routes: Routes = [
     ProveedoresComponent,
     InicioComponent,
     HeaderComponent,
-    AddproveedorComponent
+    AddproveedorComponent,
+    AddpresupuestoComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
---
+++ appCompras/src/app/presupuestos/addpresupuesto/addpresupuesto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addpresupuesto',
+  templateUrl: './addpresupuesto.component.html',
+  styleUrls: ['./addpresupuesto.component.css']
+})
+export class AddpresupuestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
