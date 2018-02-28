2d642747ffe9ffe532bf741030ac7ec6c8ed32fc
imobac-marin <imomarin@gmail.com>
2018-02-28 15:16:38 +0000

    Clase nº55

--- appCompras/src/app/autenticacion/registro/registro.component.ts
+++ appCompras/src/app/autenticacion/registro/registro.component.ts
@@ -12,6 +12,23 @@ export class RegistroComponent implements OnInit {
 
   registroForm: FormGroup;
   userdata: any;
+  erroresForm = {
+    'email': '',
+    'password': ''
+  };
+
+  mensajesValidacion = {
+    'email': {
+      'required': 'Email obligatorio.',
+      'email': 'Introduzca un email correcto.'
+    },
+    'password': {
+      'required': 'Contraseña obligatoria.',
+      'pattern': 'La contraseña debe tener al menos un número y una letra.',
+      'minlength': 'y más de 6 caracteres.'
+    }
+  };
+
   constructor(private formBuilder: FormBuilder,
     private autenticacionService: AutenticacionService,
     private router: Router,
@@ -22,6 +39,8 @@ export class RegistroComponent implements OnInit {
       'email': ['', [Validators.required, Validators.email]],
       'password': ['', [Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'), Validators.minLength(6)]]
     });
+    this.registroForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
   }
 
   onSubmit() {
@@ -36,6 +55,23 @@ export class RegistroComponent implements OnInit {
       password: this.registroForm.get('password').value
     };
     return saveUserdata;
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.registroForm) { return; }
+    const form = this.registroForm;
+    // tslint:disable-next-line:forin
+    for (const field in this.erroresForm) {
+      this.erroresForm[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.mensajesValidacion[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.erroresForm[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
 }
